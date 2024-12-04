4272ec7f3050a0226db7e0bf59e40bbf5aca4502
Sarafat Murshid <sarafatmurshid@gmail.com>
2024-12-04 12:19:52 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import ThemeToggle from "./ThemeToggle";
+import SearchFrom from "./SearchFrom";
+import Gallery from "./Gallery";
+
+const App = () => {
+  return (
+    <main>
+      <ThemeToggle />
+      <SearchFrom />
+      <Gallery />
+    </main>
+  );
+};
+export default App;
